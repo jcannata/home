62e723b85e268a2ea3aed943005f2dd51268a2b5
John Cannata <jcannata@users.noreply.github.com>
2023-10-17 17:57:50 -0700

    initial upload

---
+++ assets/entries/pages_about_index.page.379c5826.js
@@ -0,0 +1,1 @@
+import{_ as n}from"../chunks/chunk-c27b6911.js";import{c as o,o as s,F as _,a as e,b as t}from"../chunks/chunk-3c21305e.js";const a={},c=e("h1",null,"About",-1),r=e("p",null,[t("Example of app using "),e("code",null,"vite-plugin-ssr"),t(".")],-1);function l(p,i){return s(),o(_,null,[c,r],64)}const f=n(a,[["render",l]]);export{f as default};
